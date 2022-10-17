71fb444ffb39425e393e3d453dc9267151fdbe45
CleverlyCodes <cleverly.codes@gmail.com>
2022-10-17 10:59:35 +0800

    update login styles

--- screens/AuthScreen.jsx
+++ screens/AuthScreen.jsx
@@ -2,8 +2,10 @@ import {
   Text,
   View,
   StyleSheet,
-  Button,
 } from 'react-native';
+import {
+  Button,
+} from 'react-native-elements';
 import { theme } from '../constants/Theme';
 
 const styles = StyleSheet.create({
@@ -13,17 +15,25 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  appTitle: {
+    fontSize: 20,
+  },
+  loginButtonStyle: {
+    paddingHorizontal: 15,
+    marginTop: 20,
+  },
 });
 
 export default function Auth({onAuthenticate}) {
   return (
     <View style={styles.container}>
-      <Text>
-        Please authenticate first
+      <Text style={styles.appTitle}>
+        HomeMaker - Grocery v0.1
       </Text>
 
       <Button
         title="Login"
+        buttonStyle={styles.loginButtonStyle}
         onPress={onAuthenticate}
       />
     </View>
